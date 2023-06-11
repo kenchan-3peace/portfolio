bdf82a313b0fa67cad665a67904866a239443c4a
codonastyle <kenchan.3peace@gmail.com>
2023-06-11 14:36:41 +0900

    SPに対応

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ $(function(){
       var elementTop = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll > elementTop - windowHeight + 100){
+      if ($(window).width() >= 768) {
+        buffer = 100;
+      } else {
+        buffer = -200;
+      }
+      if (scroll > elementTop - windowHeight + buffer){
         $(this).addClass('scrollin');
       }
     });
